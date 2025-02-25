02fd908b6056d8c1310e2a70f1b69b345f804b70
Houleymatou CAMARA <houleymatou.camara@etu.univ-grenoble-alpes.fr>
2025-02-25 16:57:46 +0100

    Update and rename projet.js to script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+let inputEcriture =document.getElementById("inputEcriture");
+console.log(inputEcriture);
+let boutonValidation=document.getElementById("btnValiderMot");
+console.log(boutonValidation);
+let divZoneProposition = document.querySelector(".ZoneProposition");
+console.log(divZoneProposition);
