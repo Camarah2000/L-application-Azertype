26287f6a907d0e767493391293d9ff62afb6465a
Houleymatou CAMARA <houleymatou.camara@etu.univ-grenoble-alpes.fr>
2025-02-25 12:06:21 +0100

    Update projet.js

--- projet.js
+++ projet.js
@@ -13,3 +13,13 @@ if (motUtilisateur==listeMots[2]){
     score=score+1
 }
 console.log(score);
+// avec le boucle for
+const listeMots =["Cachalot", "Pétunia","Serviette"]  //déclaration d'un tableau
+score =0; //initialisation d'une variable
+for (let i = 0;i<=listeMots.length;i++)
+    let mot_tilisateur = prompt("entrez un mot contenu dans la première case du tableau:"); //demande à l'utilisateur de taper un mot
+    if (mot_utilisateur===listeMots[i]){
+    score=score+1 //le scrore est augementer de 1 
+}
+
+console.log(score);
