33d918d7b515f22f910a106ed972964983af9efc
Houleymatou CAMARA <houleymatou.camara@etu.univ-grenoble-alpes.fr>
2025-02-25 11:12:26 +0100

    Update projet.js

---
+++ projet.js
@@ -0,0 +1,8 @@
+console.log("Hello World");
+let motTapeOk = true // Essayez de mettre false à la place de true
+
+if (motTapeOk) {
+    console.log("Bravo, vous avez correctement tapé le mot")
+} else {
+    console.log("Échec, le mot n'est pas correct")
+}
